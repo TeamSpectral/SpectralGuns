18691f2f15f70630f8f6509e8caf09f96a4e0d7e
sigurd4 <sigurd-s@online.no>
2015-05-02 14:05:50 +0200

    update 1.2.3

--- src/main/java/com/spectral/spectral_guns/References.java
+++ src/main/java/com/spectral/spectral_guns/References.java
@@ -4,7 +4,7 @@ public class References
 {
 	public static final String MODID = "spectralguns";
 	public static final String NAME = "Spectral Guns";
-	public static final String VERSION = "1.2.2";
+	public static final String VERSION = "1.2.3";
 	public static final String CLIENT_PROXY_CLASS = "com.spectral.spectral_guns.proxy.ProxyClient";
 	public static final String SERVER_PROXY_CLASS = "com.spectral.spectral_guns.proxy.ProxyServer";
 	public static final String GUI_FACTORY_CLASS = "com.spectral.spectral_guns.gui.GuiFactory";
