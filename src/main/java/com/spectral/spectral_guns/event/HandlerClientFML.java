a478eb0589219dfcf2b1ba8977d50c34772542a2
sigurd4 <sigurd-s@online.no>
2015-07-06 17:58:28 +0200

    camera is kind of shaky when aiming for realism

--- src/main/java/com/spectral/spectral_guns/event/HandlerClientFML.java
+++ src/main/java/com/spectral/spectral_guns/event/HandlerClientFML.java
@@ -6,10 +6,12 @@ import java.util.HashMap;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Vec3;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -30,6 +32,13 @@ public class HandlerClientFML extends HandlerCommonFML
 {
 	// fml events for client only here!
 	
+	private float pitch = 0;
+	private float yaw = 0;
+	private float pitchMove = 0;
+	private float yawMove = 0;
+	private float lastPitchMove = 0;
+	private float lastYawMove = 0;
+	
 	public static KeyBinding WeaponReload;
 	public static KeyBinding WeaponZoom;
 	public static KeyBinding WeaponEject;
@@ -43,6 +52,94 @@ public class HandlerClientFML extends HandlerCommonFML
 		ClientRegistry.registerKeyBinding(WeaponReload);
 		ClientRegistry.registerKeyBinding(WeaponZoom);
 		ClientRegistry.registerKeyBinding(WeaponEject);
+	}
+	
+	@Override
+	@SubscribeEvent
+	public void playerUpdateEvent(PlayerTickEvent event)
+	{
+		ExtendedPlayer props = ExtendedPlayer.get(event.player);
+		
+		if(event.player.getHeldItem() != null && event.player.getHeldItem().getItem() instanceof ItemGun)
+		{
+			event.player.rotationPitch += this.pitchMove;
+			event.player.rotationYaw += this.yawMove;
+			this.pitch += this.pitchMove;
+			this.yaw += this.yawMove;
+			this.pitchMove *= 0.97;
+			this.yawMove *= 0.97;
+			
+			float f = 0.0005F;
+			if(event.player.getHeldItem() != null && event.player.getHeldItem().getItem() instanceof ItemGun)
+			{
+				f *= 2 * ItemGun.instability(event.player.getHeldItem(), event.player);
+				if(props.isZoomHeldDown)
+				{
+					f *= ItemGun.zoom(event.player.getHeldItem(), event.player, 1);
+				}
+				if(!props.isZoomHeldDown || ItemGun.getComponentsOf(event.player.getHeldItem(), ComponentScope.class).size() <= 0)
+				{
+					f /= 30;
+				}
+			}
+			else
+			{
+				f /= 30;
+			}
+			
+			Vec3 vec = Stuff.Coordinates3D.stabilize(new Vec3(Stuff.Randomization.r(f), Stuff.Randomization.r(f), 0), Stuff.Randomization.r(f));
+			float lastPitchMove = this.lastPitchMove;
+			float lastYawMove = this.lastYawMove;
+			this.lastPitchMove = ((float)vec.xCoord + lastPitchMove * 3) / 4;
+			this.lastYawMove = ((float)vec.yCoord + lastYawMove * 3) / 4;
+			
+			float max = 2;
+			float w = (float)Math.sqrt((this.pitch + this.pitchMove) * (this.pitch + this.pitchMove) + (this.yaw + this.yawMove) * (this.yaw + this.yawMove));
+			for(int i = 0; i < 20 && (this.pitch > 0 == this.pitchMove > 0 || this.yaw > 0 == this.yawMove > 0) && w > max; ++i)
+			{
+				vec = Stuff.Coordinates3D.stabilize(new Vec3(Stuff.Randomization.r(f), Stuff.Randomization.r(f), 0), Stuff.Randomization.r(f));
+				this.lastPitchMove = ((float)vec.xCoord + lastPitchMove * 3) / 4;
+				this.lastYawMove = ((float)vec.yCoord + lastYawMove * 3) / 4;
+				this.pitchMove = (this.lastPitchMove + this.pitchMove) / 2;
+				this.yawMove = (this.lastYawMove + this.yawMove) / 2;
+				w = (float)Math.sqrt((this.pitch + this.yawMove) * (this.pitch + this.yawMove) + (this.yaw + this.lastYawMove) * (this.yaw + this.lastYawMove));
+			}
+			max = 2;
+			w = (float)Math.sqrt(this.pitchMove * this.pitchMove + this.yawMove * this.yawMove);
+			if(w > max)
+			{
+				this.pitchMove /= w / max;
+				this.yawMove /= w / max;
+				vec = Stuff.Coordinates3D.stabilize(new Vec3(Stuff.Randomization.r(f), Stuff.Randomization.r(f), 0), Stuff.Randomization.r(f));
+				
+				this.lastPitchMove = ((float)vec.xCoord + lastPitchMove) / 2;
+				this.lastYawMove = ((float)vec.yCoord + lastYawMove) / 2;
+			}
+			max = 1;
+			w = (float)Math.sqrt(this.lastPitchMove * this.lastPitchMove + this.lastYawMove * this.lastYawMove);
+			if(w > max)
+			{
+				this.lastPitchMove /= w / max;
+				this.lastYawMove /= w / max;
+			}
+			this.pitchMove += this.lastPitchMove;
+			this.yawMove += this.lastYawMove;
+		}
+		else
+		{
+			this.pitchMove = 0;
+			this.yawMove = 0;
+			event.player.rotationPitch -= this.pitch;
+			event.player.rotationYaw -= this.yaw;
+			this.pitch = 0;
+			this.yaw = 0;
+		}
+		
+		float f = 0.02F;
+		event.player.rotationPitch -= this.pitch * f;
+		event.player.rotationYaw -= this.yaw * f;
+		this.pitch -= this.pitch * f;
+		this.yaw -= this.yaw * f;
 	}
 	
 	HashMap<KeyBinding, Integer> keyPressed = new HashMap<KeyBinding, Integer>();
