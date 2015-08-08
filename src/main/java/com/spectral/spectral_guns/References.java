b378ffde7772d59d452f5bd036a75f83fc3c209d
sigurd4 <sigurd-s@online.no>
2015-08-08 22:52:26 +0200

    online version checker on github

--- src/main/java/com/spectral/spectral_guns/References.java
+++ src/main/java/com/spectral/spectral_guns/References.java
@@ -5,8 +5,7 @@ public class References
 	public static final String MODID = "spectralguns";
 	public static final String NAME = "Spectral Guns";
 	public static final String VERSION = "1.3";
-	public static final String VERSION_LAST = "1.2.5";
-	public static final String GITHUB_DIR = "https://github.com/TeamSpectral/SpectralGuns/tree/dev/build/libs/";
+	public static final String VERSION_CHECK_URL = "https://github.com/TeamSpectral/SpectralGuns/blob/dev/versionChecker/versionData.txt";
 	public static final String CLIENT_PROXY_CLASS = "com.spectral.spectral_guns.proxy.ProxyClient";
 	public static final String SERVER_PROXY_CLASS = "com.spectral.spectral_guns.proxy.ProxyServer";
 	public static final String GUI_FACTORY_CLASS = "com.spectral.spectral_guns.gui.GuiFactory";
