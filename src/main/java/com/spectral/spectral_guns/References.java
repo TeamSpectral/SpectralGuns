9c0e4eeaef879dec85897c57d252bd6445848d0b
Elad Kinsbruner <eladkay@gmail.com>
2016-04-20 13:34:55 +0300

    New build

--- src/main/java/com/spectral/spectral_guns/References.java
+++ src/main/java/com/spectral/spectral_guns/References.java
@@ -4,7 +4,7 @@ public class References
 {
 	public static final String MODID = "spectralguns";
 	public static final String NAME = "Spectral Guns";
-	public static final String VERSION = "131"; //As in build (For SpectralCore)
+	public static final String VERSION = "132"; //As in build (For SpectralCore)
 	public static final String VERSION_CHECK_URL = "https://github.com/TeamSpectral/SpectralGuns/blob/dev/versionChecker/versionData.txt";
 	public static final String CLIENT_PROXY_CLASS = "com.spectral.spectral_guns.proxy.ProxyClient";
 	public static final String SERVER_PROXY_CLASS = "com.spectral.spectral_guns.proxy.ProxyServer";
